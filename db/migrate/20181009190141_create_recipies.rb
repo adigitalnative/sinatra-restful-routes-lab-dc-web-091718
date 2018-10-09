631f86d62e228fd90cea479519ca0a839eb65f1c
Jacqueline Chenault <adigitalnative@gmail.com>
2018-10-09 15:46:40 -0400

    Done.

---
+++ db/migrate/20181009190141_create_recipies.rb
@@ -0,0 +1,10 @@
+class CreateRecipies < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+      t.timestamps null: false
+    end
+  end
+end
